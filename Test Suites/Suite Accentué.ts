dd2ef1e8056d66bbac9a406e946d4448b5ee9d3a
kungnx <kung@henix.fr>
2022-10-20 12:05:48 +0200

    Added new test case with accent, spaces and dash

---
+++ Test Suites/Suite Accentué.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite Accentué</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6f5e57ae-3642-41c7-b122-f635a58581bb</testSuiteGuid>
+   <testCaseLink>
+      <guid>7f4d1dbf-89f0-439a-ad1c-b81da58d6b3f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/REF.01 POPO - CAS 10 - Cas accentué en succès</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
