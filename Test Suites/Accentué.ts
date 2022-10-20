311a8b2bc1d453b05779c7bf30d4e4751e5af360
kungnx <kung@henix.fr>
2022-10-20 13:27:51 +0200

    Forgot to save both test suites

--- Test Suites/Accentué.ts
+++ Test Suites/Accentué.ts
@@ -11,4 +11,10 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>131cc5bb-fc44-4c85-a92e-644411aed25d</testSuiteGuid>
+   <testCaseLink>
+      <guid>c4e5f644-0391-4752-8169-31ad2bb0a501</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/REF.01 POPO - CAS 10 - Cas accentué en succès</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/Suite avec espace.ts
+++ Test Suites/Suite avec espace.ts
@@ -11,4 +11,10 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>5d3b73a8-51f0-489e-8acb-eb9717345bdd</testSuiteGuid>
+   <testCaseLink>
+      <guid>9c04517d-fb52-44f0-a218-aa60b94a0733</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/REF.01 POPO - CAS 10 - Cas accentué en succès</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
